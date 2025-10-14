82c130e8aeccf453df7cd73f2b1c88ab78593775
raulpodar <raulpodar0@gmail.com>
2025-10-14 12:58:55 +0100

    basic structure

--- app/src/main/kotlin/com/raulp/cardshuffler/compose/navigation/CardShufflerNavigation.kt
+++ app/src/main/kotlin/com/raulp/cardshuffler/compose/navigation/CardShufflerNavigation.kt
@@ -24,7 +24,7 @@ fun NavGraphBuilder.cardShufflerNavigation() {
   }
 
   composable<CardShufflerScreen.CardList> {
-    CardListScreen()
+//    CardListScreen()
   }
 
   composable<CardShufflerScreen.SettingsScreen> {
